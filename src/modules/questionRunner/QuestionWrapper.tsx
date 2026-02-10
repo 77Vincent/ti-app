8feb9ea0ff2c5c8af6acaa095228f0b97b763c0b
tianqi.wen <tianqi.wen@fastretailing.com>
2026-02-10 22:25:59 +0900

    a

--- src/modules/questionRunner/QuestionWrapper.tsx
+++ src/modules/questionRunner/QuestionWrapper.tsx
@@ -12,10 +12,7 @@ export default function QuestionRunner({
 }: QuestionRunnerProps) {
   return (
     <div className="mx-auto my-auto max-w-2xl space-y-3">
-      <div className="flex flex-wrap items-center justify-between gap-2">
-        <h1 className="text-base font-semibold">
-          Question 1
-        </h1>
+      <div className="flex flex-wrap items-center justify-end gap-2">
         <div className="flex flex-wrap items-center gap-1.5">
           <Chip variant="bordered">
             {subjectId}
