6855b6948c3876ef0ca38275cd4fbe184876655b
tianqi.wen <tianqi.wen@fastretailing.com>
2026-02-10 10:19:43 +0900

    a

--- src/modules/startForm/StartForm.tsx
+++ src/modules/startForm/StartForm.tsx
@@ -2,20 +2,22 @@ import { SUBJECT_CATALOG } from "@/lib/subjects/subjects";
 
 export default function StartForm() {
   return (
-    <section className="space-y-4">
-      <h1 className="text-2xl font-medium text-center">
-        Choose the subject of your test
-      </h1>
+    <div className="card bg-base-100 shadow-sm my-auto">
+      <div className="card-body">
+        <h1 className="text-2xl font-medium text-center">
+          Choose the subject of your test
+        </h1>
 
-      <div className="">
-        {SUBJECT_CATALOG.map((subject) => (
-          <div className="badge badge-lg badge-primary"
-            key={subject.id}
-          >
-            {subject.label}
-          </div>
-        ))}
+        <div className="">
+          {SUBJECT_CATALOG.map((subject) => (
+            <div className="badge badge-lg badge-primary"
+              key={subject.id}
+            >
+              {subject.label}
+            </div>
+          ))}
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
