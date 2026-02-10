8c823e3ad3ccfe7ad2c0e1f4efc90b19241de2cf
tianqi.wen <tianqi.wen@fastretailing.com>
2026-02-10 10:00:44 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { StartForm } from "@/features/startForm";
+import { StartForm } from "@/modules/startForm";
 
 export default function Home() {
   return <main className="mx-auto flex min-h-screen w-full max-w-xl flex-col gap-4" >
