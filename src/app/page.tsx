8c1dcf5fc40600e45ab0f678333c665ffe8670dc
tianqi.wen <tianqi.wen@fastretailing.com>
2026-02-11 12:36:19 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-xl">
-      <Card isBlurred>
+    <div className="mx-auto w-full">
+      <Card isBlurred shadow="sm">
         <CardBody className="items-center gap-4 p-8 text-center">
           <h1 className="text-4xl font-medium">Learning by testing</h1>
           <Button as={Link} color="primary" href="/test">
