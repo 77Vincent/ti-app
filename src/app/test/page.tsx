ac557c06db26c6a3ed322fe48e45241e4e0dd6b9
tianqi.wen <tianqi.wen@fastretailing.com>
2026-02-24 20:22:52 +0900

    a

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,9 @@
 import { TestsPanel } from "@/app/components/testsPanel/TestsPanel";
 
 export default function TestPage() {
-  return <TestsPanel />;
+  return (
+    <section className="mx-auto max-w-3xl">
+      <TestsPanel />
+    </section>
+  )
 }
