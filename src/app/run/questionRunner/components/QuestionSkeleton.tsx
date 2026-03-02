4dd538d983c563596e54c75a150383fd8fcb8ddd
tianqi.wen <tianqi.wen@fastretailing.com>
2026-03-02 21:48:18 +0900

    a

--- src/app/run/questionRunner/components/QuestionSkeleton.tsx
+++ src/app/run/questionRunner/components/QuestionSkeleton.tsx
@@ -3,8 +3,9 @@ import { Skeleton } from "@heroui/react";
 export default function QuestionSkeleton({className}: {className?: string}) {
   return (
     <div className={`space-y-3 ${className ?? ""}`}>
-      <Skeleton className="h-12 w-full rounded-md" />
-      <div className="space-y-3 pt-2">
+      <Skeleton className="h-15 w-full rounded-md" />
+
+      <div className="space-y-5 pt-2">
         <Skeleton className="h-10 w-full rounded-md" />
         <Skeleton className="h-10 w-full rounded-md" />
         <Skeleton className="h-10 w-full rounded-md" />
